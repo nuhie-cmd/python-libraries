dbb4350f674625fc3bf553d248f79365cff64922
nuha <nuhieee@gmail.com>
2026-03-27 09:52:34 +0400

    Enhance exp4.py with comments for clarity
    Added comments to clarify user operations and calculations.

--- exp4.py
+++ exp4.py
@@ -3,7 +3,7 @@ import numpy as np
 # Temperature dataset
 temp = np.array([30, 31, 29, 32, 30, 28, 35, 33, 31, 30])
 
-while True:
+while True: #menu for user operation
     print("\n--- Climate Stability Analysis ---")
     print("1. Calculate Mean")
     print("2. Calculate Median")
@@ -12,30 +12,30 @@ while True:
     print("5. Evaluate Climate Stability")
     print("6. Exit")
 
-    choice = int(input("Enter your choice: "))
+    choice = int(input("Enter your choice: ")) #to store the input choice
 
     if choice == 1:
-        print("Mean Temperature:", np.mean(temp))
+        print("Mean Temperature:", np.mean(temp)) #performs and prints mean
 
     elif choice == 2:
-        print("Median Temperature:", np.median(temp))
+        print("Median Temperature:", np.median(temp)) #performs and prints median
 
     elif choice == 3:
-        print("Variance:", np.var(temp))
+        print("Variance:", np.var(temp)) #prforms and prints variance
 
     elif choice == 4:
-        print("Standard Deviation:", np.std(temp))
+        print("Standard Deviation:", np.std(temp)) #performs and prints standard deviation
 
-    elif choice == 5:
-        std = np.std(temp)
+    elif choice == 5: #defines conditions for climate analysis
+        std = np.std(temp) 
         if std > 2:
             print("Prediction: Irregular temperature pattern detected.")
         else:
             print("Prediction: Climate is relatively stable.")
 
-    elif choice == 6:
+    elif choice == 6: 
         print("Exiting program...")
-        break
+        break #exits from the menu
 
     else:
         print("Invalid choice! Try again.")
